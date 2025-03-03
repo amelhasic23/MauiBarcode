8d6f62b6937368ddcc3b9d5d84df98e69c248f94
Amel Hasić <amelhasic23@outlook.com>
2025-03-03 09:27:03 +0100

    Commit

---
+++ CommunityToolkitCameraPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿namespace MauiBarcode;
+
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+using CommunityToolkit.Maui.Views;
+using Microsoft.Maui.Graphics.Platform;
+using ZXing;
+using ZXing.Common;
+
+public partial class CommunityToolkitCameraPage : ContentPage
+{
+	private readonly IBarcodeReaderGeneric barcodeReader = new BarcodeReaderGeneric()
+	{
+		AutoRotate = true,
+		Options = new DecodingOptions()
+		{
+			PossibleFormats = [BarcodeFormat.QR_CODE],
+			TryHarder = true,
+			TryInverted = true,
+			PureBarcode = false
+		}
+	};
+	PeriodicTimer timer = new(TimeSpan.FromMilliseconds(3000));
+
+	public CommunityToolkitCameraPage()
+	{
+		InitializeComponent();
+	}
+
+#if !TIZEN
+	protected override async void OnAppearing()
+	{
+		base.OnAppearing();
+		ToolkitCameraView.MediaCaptured += OnMediaCaptured;
+
+		var cameras = await ToolkitCameraView.GetAvailableCameras(CancellationToken.None);
+		ToolkitCameraView.SelectedCamera = cameras.FirstOrDefault(x => x.Position != CommunityToolkit.Maui.Core.Primitives.CameraPosition.Front);
+	}
+
+	protected override void OnDisappearing()
+	{
+		ToolkitCameraView.MediaCaptured -= OnMediaCaptured;
+		base.OnDisappearing();
+	}
+
+	private async void OnMediaCaptured(object? sender, MediaCapturedEventArgs e)
+	{
+		var image = (PlatformImage)PlatformImage.FromStream(e.Media);
+		var result = barcodeReader.Decode(new ImageLuminanceSource(image));
+		if (result is null)
+		{
+			return;
+		}
+
+		await MainThread.InvokeOnMainThreadAsync(async () =>
+		{
+			await Toast.Make(result.Text, ToastDuration.Long).Show();
+		});
+	}
+#endif
+
+	private async void StartButton_OnClicked(object? sender, EventArgs e)
+	{
+		timer = new(TimeSpan.FromMilliseconds(3000));
+#if !TIZEN
+		await ToolkitCameraView.StartCameraPreview(CancellationToken.None);
+		while (await timer.WaitForNextTickAsync())
+		{
+			await ToolkitCameraView.CaptureImage(CancellationToken.None);
+		}
+#endif
+	}
+
+	private void StopButton_OnClicked(object? sender, EventArgs e)
+	{
+		timer.Dispose();
+#if !TIZEN
+		ToolkitCameraView.StopCameraPreview();
+#endif
+	}
+}
---
+++ ZxingCameraPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿namespace MauiBarcode;
+
+using Camera.MAUI;
+using Camera.MAUI.ZXing;
+using Camera.MAUI.ZXingHelper;
+using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+public partial class ZxingCameraPage : ContentPage
+{
+	public ZxingCameraPage()
+	{
+		InitializeComponent();
+		ZxingCameraView.BarCodeOptions = new BarcodeDecodeOptions
+		{
+			AutoRotate = true,
+			PossibleFormats = [BarcodeFormat.QR_CODE],
+		};
+		ZxingCameraView.BarCodeDecoder = new ZXingBarcodeDecoder();
+	}
+
+	private void ZxingCameraView_CamerasLoaded(object? sender, EventArgs e)
+	{
+		if (ZxingCameraView.NumCamerasDetected == 0)
+		{
+			return;
+		}
+
+#if WINDOWS
+		ZxingCameraView.Camera = ZxingCameraView.Cameras.FirstOrDefault();
+#else
+		ZxingCameraView.Camera = ZxingCameraView.Cameras.FirstOrDefault(x => x.Position != CameraPosition.Front);
+#endif
+	}
+
+	private async void ZxingCameraViewOnBarcodeDetected(object sender, BarcodeEventArgs args)
+	{
+		foreach (var result in args.Result)
+		{
+			await MainThread.InvokeOnMainThreadAsync(async () =>
+			{
+				await Toast.Make(result.Text, ToastDuration.Long).Show();
+			});
+		}
+	}
+
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+		ZxingCameraView.BarcodeDetected += ZxingCameraViewOnBarcodeDetected;
+		ZxingCameraView.CamerasLoaded += ZxingCameraView_CamerasLoaded;
+	}
+
+	protected override void OnDisappearing()
+	{
+		ZxingCameraView.BarcodeDetected -= ZxingCameraViewOnBarcodeDetected;
+		ZxingCameraView.CamerasLoaded -= ZxingCameraView_CamerasLoaded;
+
+		base.OnDisappearing();
+	}
+
+	private async void StartButton_OnClicked(object? sender, EventArgs e)
+	{
+		await MainThread.InvokeOnMainThreadAsync(async () =>
+		{
+			await ZxingCameraView.StartCameraAsync();
+		});
+	}
+
+	private async void StopButton_OnClicked(object? sender, EventArgs e)
+	{
+		await MainThread.InvokeOnMainThreadAsync(async () =>
+		{
+			await ZxingCameraView.StopCameraAsync();
+		});
+	}
+}
